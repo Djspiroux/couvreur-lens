018eb3ec365bb3419cc77c73ce2d58fa7eeffee6
unknown <svn.renard@gmail.com>
2024-02-28 18:19:07 +0100

    ETS-CANTON

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+
+$header="MIME-Version: 1.0\r\n";
+$header.='From:"Spiroux.com"<svn.renard@gmail.com>'."\n";
+$header.='Content-Type:text/html; charset="utf-8"'."\n";
+$header.='Content-Transfer-Encoding: 8bit';
+
+
+?>
